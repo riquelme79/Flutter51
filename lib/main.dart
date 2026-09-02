34c2a12e77d44e0ce49f8c49ccab31b9dc291c2d
Riquelme Macedo Santos <rm7629586@gmail.com>
2026-09-02 08:44:48 -0300

    Inicialização do projeto, implementação de Date e Time Picker

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'agendamento de Evento',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+
+      home: const AgendamentoEventoTela(),
+        
+   );
+  }
+}
+
+
+class AgendamentoEventoTela extends StatefulWidget{
+  const AgendamentoEventoTela({super.key});
+
+
+  @override
+    State<AgendamentoEventoTela> createState() => _AgedamentoEventoTelaState(); 
+  }
+
+
+  class _AgedamentoEventoTelaState extends State<AgendamentoEventoTela> {
+    // 1. Valores Padrão (para reset)
+    static final DateTime _dataPadrao = DateTime.now();
+    static const TimeOfDay _horarioPadrao = TimeOfDay(hour: 19, minute: 0);
+    static const String _tipoPadrao = 'Aniversário';
+
+    // 2. Variáveis de Estado
+    late DateTime _dataSelecionada;
+    late TimeOfDay _horarioSelecionado;
+    late String _tipoEventoSelecionado;
+
+    @override
+    void initState() {
+      super.initState();
+      _resetarValores();
+
+    }
+
+    void _resetarValores() {
+      setState(() {
+        _dataSelecionada = _dataPadrao;
+        _horarioSelecionado = _horarioPadrao;
+        _tipoEventoSelecionado = _tipoPadrao;
+
+      });
+      print('[DEBUG] Formulario resetado para os valores padrao.');
+    }
+
+    void _salvarFormulario() {
+      print("========================================");
+      print("          RESUMO DO AGEDAMENTO          ");
+      print("========================================");
+      print(
+        'Data: ${_dataSelecionada}/${_dataSelecionada.month}/${_dataSelecionada.year}',
+      );
+      print('Horário: ${_horarioSelecionado.format(context)}');
+      print('Tipo de Evento: $_tipoEventoSelecionado');
+      print("========================================");
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content:Text('Evento Salvo com sucesso! Veja os logs no console.'),
+        ),
+      );
+    }
+    // Funções auxiliares para Pickers
+    Future<void> _selecionarData(BuildContext context) async {
+      final DateTime? data = await showDatePicker(
+        context: context,
+        initialDate: _dataSelecionada,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2030),
+      );
+      if (data != null && data != _dataSelecionada)
+      {
+        setState(() {
+          _dataSelecionada = data;
+        });
+        print('[DEBUG - DatePicker] Data selecioanda: $data');
+      }
+    }
+
+    Future<void> _selecionarHorario(BuildContext context) async {
+      final TimeOfDay? horario = await showTimePicker(  
+        context: context,
+        initialTime: _horarioSelecionado,
+      );
+      if (horario != null && horario != _horarioSelecionado){
+        setState(() {
+          _horarioSelecionado = horario;
+        });
+        print(  
+          '[DEBUG - TimePicker] Horário selecionado: ${horario.format(context)}',
+        );
+      }
+    }
+
+    @override
+    Widget build(BuildContext context ) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Novo Evento Social'),
+          backgroundColor: Theme.of(context). colorScheme.inversePrimary,
+        ),
+        body:SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // --- 1. DatePicker & 2. TimePicker ---
+              Text(
+                'Data e Horário',
+                style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Expanded(
+                      child:ElevatedButton.icon(
+                        icon: const Icon(Icons.calendar_today),
+                        label: Text(
+                          '${_dataSelecionada.day}/${_dataSelecionada.month}/${_dataSelecionada.year}',
+                        ),
+                        onPressed: ()=> _selecionarData(context)
+                        ), 
+                        ),
+                        const SizedBox(width:12),
+                        Expanded(
+                           child:ElevatedButton.icon(
+                        icon: const Icon(Icons.access_time),
+                        label: Text(_horarioSelecionado.format(context)),
+                        onPressed: () => _selecionarHorario(context),
+                           ),
+                          ), 
+                  ],
+               ),
+               const Divider(height: 32),
+            ],
+               ),    
+          ),
+        );
+    }
+  }
+
+
+
+
+
+
+  
