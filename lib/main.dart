6da7e4fd773c108e52a2525a458120be8ee52fea
Riquelme Macedo Santos <rm7629586@gmail.com>
2026-09-09 08:29:11 -0300

    Implementado Switch para envio de lembrete automático

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'agendamento de Evento',
+      title:'agendamento de Evento',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -18,201 +18,379 @@ class MeuApp extends StatelessWidget {
       ),
 
       home: const AgendamentoEventoTela(),
-    );
+        
+   );
   }
 }
 
-class AgendamentoEventoTela extends StatefulWidget {
+
+class AgendamentoEventoTela extends StatefulWidget{
   const AgendamentoEventoTela({super.key});
 
-  @override
-  State<AgendamentoEventoTela> createState() => _AgedamentoEventoTelaState();
-}
-
-class _AgedamentoEventoTelaState extends State<AgendamentoEventoTela> {
-  // 1. Valores Padrão (para reset)
-  static final DateTime _dataPadrao = DateTime.now();
-  static const TimeOfDay _horarioPadrao = TimeOfDay(hour: 19, minute: 0);
-  static const String _tipoPadrao = 'Aniversário';
-  static const double _convidadosPadrao = 50.0;
-
-  // 2. Variáveis de Estado
-  late DateTime _dataSelecionada;
-  late TimeOfDay _horarioSelecionado;
-  late String _tipoEventoSelecionado;
-  late double _quantidadeConvidados;
 
   @override
-  void initState() {
-    super.initState();
-    _resetarValores();
+    State<AgendamentoEventoTela> createState() => _AgedamentoEventoTelaState(); 
   }
 
-  void _resetarValores() {
-    setState(() {
-      _dataSelecionada = _dataPadrao;
-      _horarioSelecionado = _horarioPadrao;
-      _tipoEventoSelecionado = _tipoPadrao;
-      _quantidadeConvidados = _convidadosPadrao;
-    });
-    print('[DEBUG] Formulario resetado para os valores padrao.');
-  }
 
-  void _salvarFormulario() {
-    print("========================================");
-    print("          RESUMO DO AGEDAMENTO          ");
-    print("========================================");
-    print(
-      'Data: ${_dataSelecionada}/${_dataSelecionada.month}/${_dataSelecionada.year}',
-    );
-    print('Horário: ${_horarioSelecionado.format(context)}');
-    print('Tipo de Evento: $_tipoEventoSelecionado');
-    print('Estimativa de Convidados: ${_quantidadeConvidados.round()}');
-    print("========================================");
+  enum Visibilidade { public, private, vip }
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Evento Salvo com sucesso! Veja os logs no console.'),
-      ),
-    );
-  }
+  class _AgedamentoEventoTelaState extends State<AgendamentoEventoTela> {
+    // 1. Valores Padrão (para reset)
+    static final DateTime _dataPadrao = DateTime.now();
+    static const TimeOfDay _horarioPadrao = TimeOfDay(hour: 19, minute: 0);
+    static const String _tipoPadrao = 'Aniversário';
+    static const double _convidadosPadrao = 50.0;
+    static const Visibilidade _visibilidadePadrao  = .private;
 
-  // Funções auxiliares para Pickers
-  Future<void> _selecionarData(BuildContext context) async {
-    final DateTime? data = await showDatePicker(
-      context: context,
-      initialDate: _dataSelecionada,
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2030),
-    );
-    if (data != null && data != _dataSelecionada) {
-      setState(() {
-        _dataSelecionada = data;
-      });
-      print('[DEBUG - DatePicker] Data selecioanda: $data');
+    static const Map<String, bool> _servicosPadrao = {
+      'Buffet': false,
+      'Fotografo': false,
+      'Decoracao': false,
+      'DJ':false,
+    };
+
+    static const List<String> _tagsDisponiveis = [
+      'Vegetariano',
+      'Sem Gluten',
+      'Sem Lactose',
+      'Vegano',
+    ];
+
+    static const List<String> _tagsPadrao = [];
+     static const bool _lembretePadrao = true;
+
+    // 2. Variáveis de Estado
+    late DateTime _dataSelecionada;
+    late TimeOfDay _horarioSelecionado;
+    late String _tipoEventoSelecionado;
+    late double _quantidadeConvidados;
+    late Visibilidade _visibilidadeSelecionada;
+    late Map<String, bool>_servicosSelecionados;
+    late List<String> _tagsSelecionadas;
+    late bool _notificacaoAtiva;
+
+    @override
+    void initState() {
+      super.initState();
+      _resetarValores();
+
     }
-  }
 
-  Future<void> _selecionarHorario(BuildContext context) async {
-    final TimeOfDay? horario = await showTimePicker(
-      context: context,
-      initialTime: _horarioSelecionado,
-    );
-    if (horario != null && horario != _horarioSelecionado) {
+    void _resetarValores() {
       setState(() {
-        _horarioSelecionado = horario;
+        _dataSelecionada = _dataPadrao;
+        _horarioSelecionado = _horarioPadrao;
+        _tipoEventoSelecionado = _tipoPadrao;
+        _quantidadeConvidados = _convidadosPadrao;
+        _visibilidadeSelecionada = _visibilidadePadrao;
+        _servicosSelecionados = Map<String, bool>.from(_servicosPadrao);
+        _tagsSelecionadas = List<String>.from(_tagsPadrao);
+        _notificacaoAtiva = _lembretePadrao;
+
       });
+      print('[DEBUG] Formulario resetado para os valores padrao.');
+    }
+
+    void _salvarFormulario() {
+      print("========================================");
+      print("          RESUMO DO AGEDAMENTO          ");
+      print("========================================");
       print(
-        '[DEBUG - TimePicker] Horário selecionado: ${horario.format(context)}',
+        'Data: ${_dataSelecionada}/${_dataSelecionada.month}/${_dataSelecionada.year}',
+      );
+      print('Horário: ${_horarioSelecionado.format(context)}');
+      print('Tipo de Evento: $_tipoEventoSelecionado');
+      print('Estimativa de Convidados: ${_quantidadeConvidados.round()}');
+      print('Visibilidade: $_visibilidadeSelecionada');
+      print('Serviços adicionais: $_servicosSelecionados');
+      print('Restrições Alimntares (Tags):$_tagsSelecionadas');
+      print('Lembrete automático: $_notificacaoAtiva');
+      print("========================================");
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content:Text('Evento Salvo com sucesso! Veja os logs no console.'),
+        ),
       );
     }
-  }
+    // Funções auxiliares para Pickers
+    Future<void> _selecionarData(BuildContext context) async {
+      final DateTime? data = await showDatePicker(
+        context: context,
+        initialDate: _dataSelecionada,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2030),
+      );
+      if (data != null && data != _dataSelecionada)
+      {
+        setState(() {
+          _dataSelecionada = data;
+        });
+        print('[DEBUG - DatePicker] Data selecioanda: $data');
+      }
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Novo Evento Social'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // --- 1. DatePicker & 2. TimePicker ---
-            Text(
-              'Data e Horário',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton.icon(
-                    icon: const Icon(Icons.calendar_today),
-                    label: Text(
-                      '${_dataSelecionada.day}/${_dataSelecionada.month}/${_dataSelecionada.year}',
-                    ),
-                    onPressed: () => _selecionarData(context),
+    Future<void> _selecionarHorario(BuildContext context) async {
+      final TimeOfDay? horario = await showTimePicker(  
+        context: context,
+        initialTime: _horarioSelecionado,
+      );
+      if (horario != null && horario != _horarioSelecionado){
+        setState(() {
+          _horarioSelecionado = horario;
+        });
+        print(  
+          '[DEBUG - TimePicker] Horário selecionado: ${horario.format(context)}',
+        );
+      }
+    }
+
+    @override
+    Widget build(BuildContext context ) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Novo Evento Social'),
+          backgroundColor: Theme.of(context). colorScheme.inversePrimary,
+        ),
+        body:SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // --- 1. DatePicker & 2. TimePicker ---
+              Text(
+                'Data e Horário',
+                style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [
+                    Expanded(
+                      child:ElevatedButton.icon(
+                        icon: const Icon(Icons.calendar_today),
+                        label: Text(
+                          '${_dataSelecionada.day}/${_dataSelecionada.month}/${_dataSelecionada.year}',
+                        ),
+                        onPressed: ()=> _selecionarData(context)
+                        ), 
+                        ),
+                        const SizedBox(width:12),
+                        Expanded(
+                           child:ElevatedButton.icon(
+                        icon: const Icon(Icons.access_time),
+                        label: Text(_horarioSelecionado.format(context)),
+                        onPressed: () => _selecionarHorario(context),
+                           ),
+                          ), 
+                  ],
+               ),
+               const Divider(height: 32),
+
+               // -- 3. Menu (Dropdownbutton) ---
+               Text(
+                'Tipo de Evento',
+                style: Theme.of(context).textTheme.titleMedium,
+               ),
+               const SizedBox(height: 8),
+               DropdownButtonFormField<String>(  
+                initialValue: _tipoEventoSelecionado,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  contentPadding:EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
                   ),
                 ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: ElevatedButton.icon(
-                    icon: const Icon(Icons.access_time),
-                    label: Text(_horarioSelecionado.format(context)),
-                    onPressed: () => _selecionarHorario(context),
-                  ),
-                ),
-              ],
-            ),
-            const Divider(height: 32),
-
-            Text(
-              'Tipo de Evento',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(height: 8),
-            DropdownButtonFormField<String>(
-              initialValue: _tipoEventoSelecionado,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 8,
-                ),
-              ),
-              items: ['Aniversário', 'Casamento', 'Corporativo', 'Outro']
-                  .map(
-                    (tipo) => DropdownMenuItem(value: tipo, child: Text(tipo)),
+                items: ['Aniversário','Casamento','Corporativo','Outro']
+                  .map((tipo) => DropdownMenuItem(value: tipo, child: Text(tipo)),
                   )
                   .toList(),
-              onChanged: (novoValor) {
-                if (novoValor != null) {
-                  setState(() {
-                    _tipoEventoSelecionado = novoValor;
-                  });
-                  print(
-                    '[DEBUG - Menu] Tipo de evento selecionado: $novoValor',
-                  );
-                }
-              },
-            ),
-            const Divider(height: 32),
+                  onChanged: (novoValor) {
+                    if (novoValor !=null){
+                      setState(() {
+                        _tipoEventoSelecionado = novoValor;
+                      });
+                      print(
+                        '[DEBUb - Menu] Tipo de evento selecionado: $novoValor',
+                      );
+                    }
+                  },
+                
+               ),
+               const Divider(height: 32),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Quantidade de Convidados',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text (
-                  '${_quantidadeConvidados.round()} pessoas',
-                  style: const TextStyle(fontWeight: FontWeight.bold), 
-                ),
-              ],
-            ),
-            Slider(
-              value: _quantidadeConvidados,
-              min: 10,
-              max: 500,
-              divisions: 49,
-              label: _quantidadeConvidados.round().toString(),
-              onChanged: (novoValor) {
+               // ---4.slider ---
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '${_quantidadeConvidados.round()} pessoas',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                ],
+                
+               ),
+               Slider(value: _quantidadeConvidados,
+               min: 10,
+               max: 500,
+               divisions: 49,
+               label: _quantidadeConvidados.round().toString(),
+               onChanged: (novoValor){
                 setState(() {
                   _quantidadeConvidados = novoValor;
                 });
                 print(
                   '[DEBUG - Slider] Quantidade de convidados: ${novoValor.round()}',
                 );
-              },
-            ),
-            const Divider(height: 32),
-          ],
-        ),
-      ),
-    );
+               },
+              ),
+              const Divider(height: 32),
+
+
+              // --- 5. radio ---
+              Text(
+                'Visibilidade do Eveto',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+
+              RadioGroup<Visibilidade>(
+                groupValue: _visibilidadeSelecionada,
+                onChanged: (Visibilidade? visibilidade) {
+                  setState(() {
+                    _visibilidadeSelecionada = visibilidade!;
+                    print('[DEBUG - Radio] Visibilidade:$visibilidade');
+                  });
+                },
+               child:Column(
+                children: [
+                  ListTile(
+                    title: Text('Público'),
+                    leading: Radio<Visibilidade>(value: Visibilidade.public),
+                  ),
+
+                  ListTile(
+                    title: Text('Privado'),
+                    leading: Radio<Visibilidade>(value: Visibilidade.private),
+                  ),
+
+                  ListTile(
+                    title: Text('Apenas Convidados'),
+                    leading: Radio<Visibilidade>(value: Visibilidade.vip),
+                  ),
+                ],
+               ),
+               ),
+               const Divider(height: 32),
+
+               // --- 6 checkbox ---
+
+               Text(
+                'Serviços Adicionais',
+                style: Theme.of(context).textTheme.titleMedium,
+               ),
+               Column(
+                children:_servicosSelecionados.keys.map((servico){
+                  return CheckboxListTile(
+                    dense: true,
+                    title: Text(servico),
+                    value: _servicosSelecionados[servico],
+                    onChanged: (bool? marcado) {
+                      setState(() {
+                        _servicosSelecionados[servico] = marcado ?? false;
+                      });
+                      print(
+                        '[DEBUG - Checkbox] Serviço "$servico" alterado para: $marcado',
+                        );
+                    },
+                    );
+                }).toList(),
+               ),
+              const Divider(height: 32),
+
+
+              // --- 7. Chip ---
+               Text(
+                'Restrições alimantares (Tags)',
+                style: Theme.of(context).textTheme.titleMedium,
+               ),
+              const SizedBox(height: 8),
+              Wrap(
+                spacing: 8.0,
+                children:_tagsDisponiveis.map((tag) {
+                  final estaSelecionado = _tagsSelecionadas.contains(tag);
+                  return FilterChip(
+                    label: Text(tag), 
+                    selected: estaSelecionado,
+                    onSelected:(bool selecionado) {
+                      setState(() {
+                        if(selecionado){
+                          _tagsSelecionadas.add(tag);
+                        } else {
+                          _tagsSelecionadas.remove(tag);
+                        }
+                      });
+                      print('[DEBUG - Chip] Tag "$tag" ${selecionado ? "adicionada" : "removida"}. lista atual: $_tagsSelecionadas',
+                      );
+                    }
+                    );
+                }).toList(),
+              ),
+              const Divider(height: 32),
+              // ---- 8. Switch ---
+
+              SwitchListTile(
+                title: const Text('enviar Lembrete Autmático'),
+                subtitle: const Text(
+                  'Notificar convidados 24 horas antes do evento',
+                ),
+                value: _notificacaoAtiva, 
+                onChanged: (bool ativo){
+                  setState(() {
+                    _notificacaoAtiva = ativo;
+                  });
+                  print('[DEBUG - Switch] Notificção automática alterada para: $ativo',
+                  );
+                },
+                ),
+                 const Divider(height: 32),
+
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed:_resetarValores ,
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.red,
+                          side: const BorderSide( color:Colors.red,)
+                           ),
+                            child: const Text('Cancelar'),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed:_salvarFormulario ,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).colorScheme.primary,
+                          foregroundColor: Colors.white,
+                           ),
+                            child: const Text('Salvar'),
+                      ),
+                    ),
+                  ],
+                )
+
+            ],
+          )
+        )
+      );
+    }
   }
-}
+
+
+
+
+
+
+  
